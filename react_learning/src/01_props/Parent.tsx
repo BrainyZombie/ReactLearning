16844305cd1987990dc7eb07c02dcef29497908a
ashir basu <ashirbasu@gmail.com>
2022-02-27 23:53:06 +0530

    Setup for Redux section and cleanup

--- react_learning/src/01_props/Parent.tsx
+++ react_learning/src/01_props/Parent.tsx
@@ -8,7 +8,7 @@ const Parent = () => {
       {/* <Child color="red" onClick={() => console.log("Child Clicked")}>
         <div>Child can't have children</div>
       </Child> */}
-      <hr></hr>
+      <br />
       <ChildAsFC color="red" onClick={() => console.log("ChildAsFC Clicked")}>
         <div>ChildAsFC can have children</div>
       </ChildAsFC>
--- react_learning/src/index.tsx
+++ react_learning/src/index.tsx
@@ -6,26 +6,40 @@ import UserSearch from "./02_state/UserSearch";
 import EventComponent from "./03_events/EventComponent";
 import UserSearchClass from "./04_classes/UserSearchClass";
 import UserSearchRef from "./06_refs/UserSearchRef";
+import { useState } from "react";
+
 const App = () => {
+  const [isDemo, setIsDemo] = useState(true);
+  let toShow = <div />;
   return (
     <div>
-      <Parent />
-      <Spacer />
-      <GuestList />
-      <Spacer />
-      <UserSearch />
-      <Spacer />
-      <EventComponent />
-      <Spacer />
-      <UserSearchClass
-        users={[
-          { name: "sara", age: 22 },
-          { name: "john", age: 30 },
-          { name: "jane", age: 27 },
-        ]}
-      />
-      <Spacer />
-      <UserSearchRef />
+      <button onClick={() => setIsDemo(!isDemo)}>Toggle Demo</button>
+      {isDemo ? (
+        <>
+          <h2>Practice Demo</h2>
+          <Parent />
+          <Spacer />
+          <GuestList />
+          <Spacer />
+          <UserSearch />
+          <Spacer />
+          <EventComponent />
+          <Spacer />
+          <UserSearchClass
+            users={[
+              { name: "sara", age: 22 },
+              { name: "john", age: 30 },
+              { name: "jane", age: 27 },
+            ]}
+          />
+          <Spacer />
+          <UserSearchRef />
+        </>
+      ) : (
+        <>
+          <h2>Redux Demo</h2>
+        </>
+      )}
     </div>
   );
 };
